2480e8223fb7945902db8dfe6a0c333e1c852551
Katser <gray84@tut.by>
2020-06-26 06:49:01 +0300

    Готов index.html

--- assets/js/script.mjs
+++ assets/js/script.mjs
@@ -89,17 +89,37 @@
 /***/ (function(module, exports) {
 
 window.onload = () => {
-  new Glide('.glide', {
-    type: 'carousel',
-    perView: 2,
-    gap: 15,
-    autoplay: 3000,
-    breakpoints: {
-      768: {
-        perView: 1
+  const _listeners = () => {
+    const forActive = document.querySelectorAll('.for-active');
+    forActive.forEach(item => {
+      item.onclick = e => {
+        e.preventDefault();
+        item.classList.toggle('selected');
+      };
+    });
+  };
+
+  const _glide = () => {
+    new Glide('.glide', {
+      type: 'carousel',
+      perView: 2,
+      gap: 15,
+      // autoplay: 3000,
+      breakpoints: {
+        768: {
+          perView: 1
+        }
       }
-    }
-  }).mount();
+    }).mount();
+  };
+
+  const init = () => {
+    _listeners();
+
+    _glide();
+  };
+
+  init();
 };
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,29 @@
-
 window.onload = () => {
-	new Glide('.glide',{
-		type: 'carousel',
-		perView: 2,
-		gap: 15,
-		autoplay: 3000,
-		breakpoints: {
-			768: {
-				perView: 1
+	const _listeners = () => {
+		const forActive = document.querySelectorAll('.for-active')
+		forActive.forEach(item => {
+			item.onclick = (e) => {
+				e.preventDefault()
+				item.classList.toggle('selected')
 			}
-		}
-	}).mount()
+		});
+	}
+	const _glide = () => {
+		new Glide('.glide', {
+			type: 'carousel',
+			perView: 2,
+			gap: 15,
+			autoplay: 3000,
+			breakpoints: {
+				768: {
+					perView: 1
+				}
+			}
+		}).mount()
+	}
+	const init = () => {
+		_listeners()
+		_glide()
+	}
+	init()
 }
